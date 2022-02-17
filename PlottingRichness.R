cde0c82be3689e096daf9888759e373b118f31ce
steigeec <40217381+steigeec@users.noreply.github.com>
2022-02-17 14:13:41 -0800

    Create PlottingRichness.R

---
+++ PlottingRichness.R
@@ -0,0 +1,59 @@
+#Project: Kipukas               #
+#Script: PlottingRichness.R     #
+#Author: Emma Steigerwald       #
+#Date:17 Feb 2022               #
+#################################
+
+#Set up my working environment
+setwd("G:/My Drive/Kipuka")
+library(ggplot2)
+library(extrafont)
+font_import()
+
+
+richness <- read.csv("merged_by_site_2.csv")
+
+#Establish some color schemes up top to apply to all
+#Colors are from color-blind friendly, rcartocolor "Safe" palette
+SiteColors <- c("Center" = "#332288", "Edge" = "#6699CC", "Lava"="#888888", "Kona"="#117733", "Stainbeck"="#999933")
+#Establish some themes up top to apply to all
+KipukaTheme <- theme(axis.title=element_text(size=30), 
+        axis.text = element_text(size=25), 
+        plot.margin = unit(c(0, 0, 0, 0), "cm"), 
+        plot.title=element_text(size=30), 
+        legend.text=element_text(size=25), 
+        legend.key.height = unit(1, "cm"), 
+        legend.key.width = unit(1.5,"cm"), 
+        panel.background = element_blank(), 
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(), 
+        plot.background = element_blank(), 
+        text = element_text(family = "serif")) 
+
+#Create an NMDS plot with columns MDS1 and MDS2
+richness_mod <- richness
+richness_mod$Arealog[richness_mod$Site=="Lava" & is.na(richness_mod$Arealog)] <- 1.5
+richness_mod$Arealog[richness_mod$Site=="Kona" & is.na(richness_mod$Arealog)] <- 8
+richness_mod$Arealog[richness_mod$Site=="Stainbeck" & is.na(richness_mod$Arealog)] <- 8
+richness_mod$Arealog<-richness_mod$Arealog*2
+
+jpeg("Figures/NMDS.jpg", width=1000, height=1000)
+ggplot() + 
+  geom_point(data=richness_mod,aes(x=MDS1,y=MDS2,colour=Site, size=(Arealog)), alpha=0.5) + 
+  #geom_polygon(data=hull.data,aes(x=MDS1,y=MDS2,fill=grp,group=grp),alpha=0.30) + # add the convex hulls
+  scale_colour_manual(values=SiteColors) +
+  scale_size_continuous(labels = c("Lava", "3", "4", "5", "6", "7", "Continuous forest"), range=c(3, 16), breaks=c(3, 6, 8, 10, 12, 14, 16)) +
+  labs(title="NMDS plot", x="NMDS1", y="NMDS2") +
+  coord_equal() +
+  guides(colour = guide_legend(override.aes = list(size=10))) + 
+  KipukaTheme +
+  theme(panel.grid.major = element_line(
+        rgb(105, 105, 105, maxColorValue = 255),
+        linetype = "dotted", 
+        size=1),   
+      panel.grid.minor = element_line(
+        rgb(105, 105, 105, maxColorValue = 255),
+        linetype = "dotted", 
+        size = 0.5))
+dev.off()
+
