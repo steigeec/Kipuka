b320301a10e416cf2547fdd44ecc3b76dfe62acd
steigeec <40217381+steigeec@users.noreply.github.com>
2022-02-19 00:20:27 -0800

    Update KipukaMaps.R

--- KipukaMaps.R
+++ KipukaMaps.R
@@ -12,15 +12,39 @@ library(sp)
 library(rgdal)
 library(raster)
 font_import()
+library(viridis)
+
+SiteColors <- c("Center" = "#332288", "Edge" = "#6699CC", "Lava"="#888888", "Kona"="#117733", "Stainbeck"="#999933")
+#Establish some themes up top to apply to all
+KipukaTheme <- theme(axis.title=element_text(size=30), 
+        axis.text = element_text(size=25), 
+        plot.margin = unit(c(0, 0, 0, 0), "cm"), 
+        legend.text=element_text(size=25), 
+        legend.key.height = unit(1, "cm"), 
+        legend.key.width = unit(1.5,"cm"), 
+        panel.background = element_blank(), 
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(), 
+        plot.background = element_blank(), 
+        legend.background = element_blank(),
+        legend.title = element_text(size=25), 
+        text = element_text(family = "serif")) 
+
+
 
 #First, rainfall showing inset site polygons
 
-rain<-raster("AET_mm_ann_hr_raster")
-#rain <- readOGR("Mapping/Annual_Rainfall_(mm)/Annual_Rainfall_(mm).shp") 
+rain<-raster("Mapping/HawaiiRFGrids_mm/rf_mm_bi_ann/w001001.adf")
+rain_spdf <- as(rain, "SpatialPixelsDataFrame")
+rain_df <- as.data.frame(rain_spdf)
+colnames(rain_df) <- c("value", "x", "y")
 
-ggplot() + 
-  geom_polygon(data=rain, aes(long, lat))  #, group = group, fill = hole 
-
+jpeg("Figures/BigIsland.jpg", width=1000, height=1000)
+ggplot() +  
+  geom_tile(data=rain_df, aes(x=x, y=y, fill=value^.5)) +  
+  scale_fill_viridis_c(direction=-1, breaks=c(2000^.5, 4000^.5, 6000^.5), labels=c(2000, 4000, 6000)) +
+  KipukaTheme
+dev.off()
 
 
 coords <- read.csv("merged_by_site_2.csv")
