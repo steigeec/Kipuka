74eded58254773c396b9fd61f571f2091674f090
steigeec <40217381+steigeec@users.noreply.github.com>
2022-02-18 22:43:07 -0800

    Update PlottingRichness.R

--- PlottingRichness.R
+++ PlottingRichness.R
@@ -28,23 +28,27 @@ KipukaTheme <- theme(axis.title=element_text(size=30),
         panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(), 
         plot.background = element_blank(), 
+        legend.background = element_blank(),
+        legend.title = element_text(size=25), 
         text = element_text(family = "serif")) 
 
 #Create an NMDS plot with columns MDS1 and MDS2
 richness_mod <- richness
-richness_mod$Arealog[richness_mod$Site=="Lava" & is.na(richness_mod$Arealog)] <- 1.5
-richness_mod$Arealog[richness_mod$Site=="Kona" & is.na(richness_mod$Arealog)] <- 10
-richness_mod$Arealog[richness_mod$Site=="Stainbeck" & is.na(richness_mod$Arealog)] <- 10
-richness_mod$Arealog<-richness_mod$Arealog*2
+richness_mod$Arealog[richness_mod$Site=="Lava" & is.na(richness_mod$Arealog)] <- 3
+richness_mod$Arealog[richness_mod$Site=="Kona" & is.na(richness_mod$Arealog)] <- 9
+richness_mod$Arealog[richness_mod$Site=="Stainbeck" & is.na(richness_mod$Arealog)] <- 9
+richness_mod$Arealog<-richness_mod$Arealog^1.5
 
-jpeg("Figures/NMDS.jpg", width=1000, height=1000)
+jpeg("Figures/NMDS_1.jpg", width=1000, height=1000)
 ggplot() + 
-  geom_point(data=richness_mod,aes(x=MDS1,y=MDS2,colour=Site, size=(Arealog)), alpha=0.5) + 
+  geom_point(data=richness_mod,aes(x=MDS1,y=MDS2,colour=Site, size=(Arealog), shape=Site), alpha=0.65) + 
   #geom_polygon(data=hull.data,aes(x=MDS1,y=MDS2,fill=grp,group=grp),alpha=0.30) + # add the convex hulls
   scale_colour_manual(values=SiteColors) +
-  scale_size_continuous(labels = c("Lava", "3", "4", "5", "6", "7", "Continuous forest"), range=c(3, 20), breaks=c(3, 6, 8, 10, 12, 14, 20)) +
+  scale_shape_manual("Site", values=c("Center" = 16, "Edge" = 16, "Lava"=3, "Kona"=16, "Stainbeck"=16)) +
+  scale_size_continuous("Log area ("~km^2~")", labels = c("3", "4", "5", "6", "7", "Continuous forest"), range=c(5.20,27), breaks=c(5.20, 8, 13.13, 14.70, 18.52, 27)) +
   labs(title="NMDS plot", x="NMDS1", y="NMDS2") +
   coord_equal() +
+  scale_y_continuous(limits=c(-1,1.20)) +
   guides(colour = guide_legend(override.aes = list(size=10))) + 
   KipukaTheme +
   theme(panel.grid.major = element_line(
