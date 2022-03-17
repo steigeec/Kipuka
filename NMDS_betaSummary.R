a37778e3a8c68226d010e53ff1d3e2e1ceeae929
steigeec <40217381+steigeec@users.noreply.github.com>
2022-03-17 11:54:24 -0700

    Update and rename NMDS.R to NMDS_betaSummary.R

---
+++ NMDS_betaSummary.R
@@ -0,0 +1,183 @@
+#Project: Kipukas               #
+#Script: NMDS.R                 #
+#Author: Emma Steigerwald       #
+#Date:17 Feb 2022               #
+#################################
+
+#Set up my working environment
+setwd("G:/My Drive/Kipuka")
+library(ggplot2)
+library(extrafont)
+library(reshape2)
+library(cowplot)
+library(tidyverse)
+font_import()
+
+
+richness <- read.csv("merged_by_site_2.csv")
+dist_beta_0 <- read.csv("Distance_v_beta.csv")
+otu <- read.csv("3OTU.csv")
+dist_diff <- read.csv("Distance_v_differentiation.csv")
+geo_dist<-read.csv("geo_dist.csv")
+
+#Establish some color schemes up top to apply to all
+#Colors are from color-blind friendly, rcartocolor "Safe" palette
+SiteColors <- c("Center" = "#332288", "Edge" = "#6699CC", "Lava"="#888888", "Kona"="#117733", "Stainbeck"="#999933")
+#Establish some themes up top to apply to all
+KipukaTheme <- theme(axis.title=element_text(size=30), 
+        axis.text = element_text(size=25, angle=45), 
+        plot.margin = unit(c(0, 0, 0, 0), "cm"), 
+        plot.title=element_text(size=30), 
+        legend.text=element_text(size=25), 
+        legend.key.height = unit(1, "cm"), 
+        legend.key.width = unit(1.5,"cm"), 
+        panel.background = element_blank(), 
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(), 
+        plot.background = element_blank(), 
+        legend.background = element_blank(),
+        legend.title = element_text(size=25), 
+        text = element_text(family = "serif"), 
+        legend.box.background = element_rect(fill = "white", color = "black"), 
+        legend.spacing.y = unit(0.1,"cm")) 
+
+                   
+#################################################################################
+#NMDS plot
+
+#Create an NMDS plot with columns MDS1 and MDS2
+richness_mod <- richness
+richness_mod$Area<-as.numeric(gsub(",","",as.character(richness_mod$Area)))
+richness_mod$pointsize<-round(sqrt(richness_mod$Area)/10,0)
+richness_mod$pointsize[richness_mod$Site=="Lava" & is.na(richness_mod$pointsize)] <- 2
+richness_mod$pointsize[richness_mod$Site=="Kona" & is.na(richness_mod$pointsize)] <- 2
+richness_mod$pointsize[richness_mod$Site=="Stainbeck" & is.na(richness_mod$pointsize)] <- 2
+
+b<- ggplot() + 
+  geom_point(data=richness_mod,aes(x=MDS1,y=MDS2,colour=Site, size=pointsize, shape=Site), alpha=0.70, stroke=3) + 
+  scale_colour_manual(values=SiteColors) +
+  scale_shape_manual("Site", values=c("Center" = 16, "Edge" = 16, "Lava"=3, "Kona"=2, "Stainbeck"=2)) +
+  scale_size_continuous("Kipuka area ("~m^2~")", range=c(2,32), breaks=seq(2,32,5), labels=round((10*seq(2,32,5))^2,100)) +
+  labs(title="B. ", x="NMDS1", y="NMDS2") +
+  #coord_equal() +
+  scale_y_continuous(limits=c(-1,1.20)) +
+  guides(colour = guide_legend(override.aes = list(size=4))) + 
+  KipukaTheme +
+  theme(panel.grid.major = element_line(
+        rgb(105, 105, 105, maxColorValue = 255),
+        linetype = "dotted", 
+        size=1),   
+      panel.grid.minor = element_line(
+        rgb(105, 105, 105, maxColorValue = 255),
+        linetype = "dotted", 
+        size = 0.5),
+        plot.title=element_text(size=50))
+
+
+######################################################33
+#Beta diversity summary plot (C)
+
+#Wrange geo distances
+rownames(geo_dist) <- geo_dist[,1]
+geo_dist <- geo_dist[,-1]
+#remove x from all the column names
+names(geo_dist)<-sub("X*", "", names(geo_dist))
+geo_dist<-as.matrix(geo_dist)
+geo_dist<-data.frame(col=colnames(geo_dist)[col(geo_dist)], row=rownames(geo_dist)[row(geo_dist)], dist=c(geo_dist))
+#make an index column that reps this particular combination of sites
+geo_dist$index<-paste(geo_dist$col, geo_dist$row, sep="_")
+geo_dist$log_dist<-log(geo_dist$dist+0.00001)
+geo_dist<-geo_dist[,c(4, 5)]
+
+#Grab 3% OTU beta diversity metrics
+acari_beta<-as.matrix(otu)
+#Remove all the XXXX from colnames
+colnames(acari_beta)<- gsub('[X]', '', colnames(acari_beta))
+rownames(acari_beta)<-acari_beta[,1]
+acari_beta<-acari_beta[,-1]
+acari_beta<-data.frame(col=colnames(acari_beta)[col(acari_beta)], row=rownames(acari_beta)[row(acari_beta)], dist=c(acari_beta))
+#Add attributes of each site
+#First we add whether it's center, edge, etc etc etc
+acari_beta<-merge(acari_beta, richness[,c(1,9)], by.x="col", by.y="ï..ID")
+acari_beta<-merge(acari_beta, richness[,c(1,9)], by.x="row", by.y="ï..ID")
+#If Site.x and Site.y are not the same (e.g. not both Center and Center), then throw out that row
+acari_beta<-acari_beta[acari_beta$Site.x==acari_beta$Site.y,]
+
+#Now add the distances between these sites
+acari_beta$index<-paste(acari_beta$row, acari_beta$col, sep="_")
+acari_beta<-merge(acari_beta, geo_dist, by.x="index", by.y="index")
+#remove the same-site pairs
+acari_beta<-acari_beta[acari_beta$row!=acari_beta$col,]     
+#Remove flipped pairs
+acari_beta <- acari_beta%>% distinct(dist, .keep_all= TRUE)
+acari_beta$dist<-as.numeric(acari_beta$dist)
+acari_beta<-acari_beta[acari_beta$Site.x!="Lava",]
+
+#Maybe join together tables of each part...?
+dist_beta <- dist_beta_0[,1:6]
+i<-c(3, 4, 5, 6)
+dist_beta[ , i] <- apply(dist_beta[ , i], 2,            # Specify own function within apply
+                    function(x) as.numeric(as.character(x)))
+
+Center <- dist_beta[!is.na(dist_beta$Center),]
+Center$site <- "Center"
+Center <- rename(Center, beta = Center)
+Center <- Center[ , colSums(is.na(Center)) < nrow(Center)]                    
+                         
+Stainbeck <- dist_beta[!is.na(dist_beta$Stainbeck),]
+Stainbeck$site <- "Stainbeck"  
+Stainbeck <- rename(Stainbeck, beta = Stainbeck)
+Stainbeck <- Stainbeck[ , colSums(is.na(Stainbeck)) < nrow(Stainbeck)]   
+                         
+Kona <- dist_beta[!is.na(dist_beta$Kona),]
+Kona$site <- "Kona"
+Kona <- rename(Kona, beta = Kona)
+Kona <- Kona[ , colSums(is.na(Kona)) < nrow(Kona)]   
+                         
+Edge <- dist_beta[!is.na(dist_beta$Edge),]
+Edge$site <- "Edge"
+Edge <- rename(Edge, beta = Edge)
+Edge <- Edge[ , colSums(is.na(Edge)) < nrow(Edge)] 
+                         
+dist_beta <- rbind(Center, Stainbeck, Kona, Edge)
+dist_beta <- rename(dist_beta, dist = ï..dist)                         
+
+
+#Now summarize beta diversity between site types
+acari_beta<-acari_beta[,c(4, 5, 7)]
+acari_beta$metric <- "3% OTU"
+dist_beta<-dist_beta[,c(2, 3, 4)]
+dist_beta$metric <- "zOTU"
+names(dist_beta)<-c( "log_dist", "dist", "Site.x", "metric")
+
+beta <- rbind(dist_beta, acari_beta) 
+#Reorder facets
+beta$metric <- factor(beta$metric, levels = rev(c("zOTU", "3% OTU")))                                                          
+                   
+c<- ggplot() + 
+  geom_boxplot(data=beta,aes(x=reorder(Site.x, dist), y=dist, fill=Site.x), color="black", size=1)+
+  facet_wrap(~metric, scales="free") +
+  scale_fill_manual(values=SiteColors) +
+  labs(title="C.", x="") +
+  KipukaTheme +
+  theme(strip.text = element_text(size = 30), 
+        axis.text = element_text(angle=45, size=40), 
+        axis.title.y = element_blank(), 
+        panel.grid.major = element_line(
+        rgb(105, 105, 105, maxColorValue = 255),
+        linetype = "dotted", 
+        size=1),   
+      panel.grid.minor = element_line(
+        rgb(105, 105, 105, maxColorValue = 255),
+        linetype = "dotted", 
+        size = 0.5), 
+       axis.title=element_text(size=50), 
+        plot.title=element_text(size=50), 
+        legend.text=element_text(size=45), 
+        legend.title = element_blank(),
+       legend.position = "top", 
+        plot.margin = margin(0.2,1,0,1.35, "cm"))
+
+jpeg("Figures/BetaSummary_NMDS.jpg", width=2000, height=1000) 
+plot_grid(b,c)                         
+dev.off()
