f87d752c14194abf512a6c5a3ecaed8b00d21713
steigeec <40217381+steigeec@users.noreply.github.com>
2023-02-02 18:41:39 -0800

    Update NMDS_betaSummary.R

--- NMDS_betaSummary.R
+++ NMDS_betaSummary.R
@@ -139,7 +139,7 @@ acari_beta<-as.matrix(otu)
 colnames(acari_beta)<- gsub('[X]', '', colnames(acari_beta))
 rownames(acari_beta)<-acari_beta[,1]
 acari_beta<-acari_beta[,-1]
-acari_beta<-data.frame(col=colnames(acari_beta)[col(acari_beta)], row=rownames(acari_beta)[row(acari_beta)], dist=c(acari_beta))
+acari_beta<-data.frame(col=colnames(acari_beta)[col(acari_beta)], row=rownames(acari_beta)[row(acari_beta)], beta=c(acari_beta))
 #Add attributes of each site
 #First we add whether it's center, edge, etc etc etc
 acari_beta<-merge(acari_beta, richness[,c(1,9)], by.x="col", by.y="ï..ID")
@@ -153,8 +153,8 @@ acari_beta<-merge(acari_beta, geo_dist, by.x="index", by.y="index")
 #remove the same-site pairs
 acari_beta<-acari_beta[acari_beta$row!=acari_beta$col,]     
 #Remove flipped pairs
-acari_beta <- acari_beta%>% distinct(dist, .keep_all= TRUE)
-acari_beta$dist<-as.numeric(acari_beta$dist)
+acari_beta <- acari_beta%>% distinct(beta, .keep_all= TRUE)
+acari_beta$beta<-as.numeric(acari_beta$beta)
 
 #Maybe join together tables of each part...?
 dist_beta <- dist_beta_0[,1:7]
@@ -162,6 +162,7 @@ i<-c(3, 4, 5, 6, 7)
 dist_beta[ , i] <- apply(dist_beta[ , i], 2,            # Specify own function within apply
                     function(x) as.numeric(as.character(x)))
 
+#So it's dist_beta column two in these following items that are the problem...                          
 Center <- dist_beta[!is.na(dist_beta$Center),]
 Center$site <- "Center"
 Center <- rename(Center, c("beta"="Center"))
@@ -196,7 +197,7 @@ acari_beta<-acari_beta[,c(4, 5, 7)]
 acari_beta$metric <- "3% OTU"
 dist_beta<-dist_beta[,c(2, 3, 4)]
 dist_beta$metric <- "zOTU"
-names(dist_beta)<-c("dist", "log_dist", "Site.x", "metric")
+names(dist_beta)<-c("log_dist", "beta", "Site.x", "metric")
 
 beta <- rbind(dist_beta, acari_beta) 
 #Reorder facets
@@ -212,7 +213,7 @@ beta$Site.x <- factor(beta$Site.x, levels=c("Lava", "Edge", "Center", "Stainback
                          
                          
 b<- ggplot() + 
-  geom_boxplot(data=beta[beta$metric=="3% OTU" & !is.na(beta$Site.x),],aes(x=Site.x, y=dist, fill=Site.x), color="black", size=1)+
+  geom_boxplot(data=beta[beta$metric=="3% OTU" & !is.na(beta$Site.x),],aes(x=Site.x, y=beta, fill=Site.x), color="black", size=1)+
   #facet_wrap(~metric, scales="free") +
   scale_fill_manual(values=SiteColors) +
   labs(title="B.", y="3% OTU beta diversity", x="") +
@@ -237,55 +238,16 @@ b<- ggplot() +
        legend.position = "top", 
         plot.margin = margin(0.2,1,0,1.35, "cm"))
 
-#######################################################
-#Bray curtis center-edge pairs only
-CE<-merge(CE, richness, by.x="log_dist", by.y="ï..ID")
-
-#Fix spelling error on sheet before proceeding
-CE$Site<-gsub("Stainbeck","Stainback",as.character(CE$Site))                         
-                         
-c<-ggplot(data=CE[CE$metric=="3% OTU",]) + 
-  geom_smooth(method='lm', aes(x=Area, y=dist), colour="black", size=1, alpha=0.20)+
-  geom_point(aes(x=Area, y=dist), colour="#6699CC", fill="#332288", alpha=0.70, size=8, shape=21, stroke=7) + 
-  labs(title="C.", x="Kipuka area ("~m^2~")", y="3% OTU beta diversity") +
-  KipukaTheme +
-  #coord_cartesian(ylim=c(0.4, 1))+
-  guides(colour="none")+
-  scale_x_continuous(trans='log10',
-                     breaks=trans_breaks('log10', function(x) 10^x),
-                     labels=trans_format('log10', math_format(10^.x)))  +                   
-  theme(strip.text = element_text(size = 45), 
-        panel.grid.major = element_line(
-        rgb(105, 105, 105, maxColorValue = 255),
-        linetype = "dotted", 
-        size=1),   
-      panel.grid.minor = element_line(
-        rgb(105, 105, 105, maxColorValue = 255),
-        linetype = "dotted", 
-        size = 0.5), 
-       axis.title=element_text(size=45), 
-        axis.text = element_text(size=40), 
-        plot.title=element_text(size=45), 
-        legend.text=element_text(size=40), 
-        legend.title = element_text(size=40),
-       legend.position = "top")                         
-
-
-#######################################################
-jpeg("Figures/NMDS-turnover.jpg", width=3000, height=1000) 
-plot_grid(a,b,c, nrow=1, rel_widths=c(1.3, .9, 1), rel_heights=c(1,1,1))                         
-dev.off()
-
-                     
+                    
 #################################################################################
 #NMDS plot for zOTU
 
-a <- ggplot() + 
+c <- ggplot() + 
   geom_point(data=nmds[nmds$Site!=c("C-E", "C-F"),],aes(x=MDS1zOTU,y=MDS2zOTU,colour=Site, size=pointsize, shape=Site), alpha=0.70, stroke=3) + 
   scale_colour_manual(values=SiteColors, limits=c("Center", "Edge", "Lava", "Kona", "Stainback")) +
   scale_shape_manual("Site", values=c("Center" = 16, "Edge" = 16, "Lava"=3, "Kona"=2, "Stainback"=2)) +
   scale_size_continuous("Kipuka area ("~m^2~")", range=c(2,32), breaks=seq(2,32,5), labels=round((10*seq(2,32,5))^2,100)) +
-  labs(title="A.", x="NMDS1", y="NMDS2") +
+  labs(title="C.", x="NMDS1", y="NMDS2") +
   #coord_equal() +
   scale_x_continuous(breaks=seq(-2,1.5,0.5)) +
   guides(colour = guide_legend(override.aes = list(size=4))) + 
@@ -300,11 +262,11 @@ a <- ggplot() +
         size = 0.5),
         plot.title=element_text(size=50))                     
                      
-b<- ggplot() + 
-  geom_boxplot(data=beta[beta$metric=="zOTU" & !is.na(beta$Site.x),],aes(x=Site.x, y=dist, fill=Site.x), color="black", size=1)+
+d<- ggplot() + 
+  geom_boxplot(data=beta[beta$metric=="zOTU" & !is.na(beta$Site.x),],aes(x=Site.x, y=beta, fill=Site.x), color="black", size=1)+
   #facet_wrap(~metric, scales="free") +
   scale_fill_manual(values=SiteColors) +
-  labs(title="B.", y="zOTU beta diversity", x="") +
+  labs(title="D.", y="zOTU beta diversity", x="") +
   KipukaTheme +
   guides(fill="none")+                       
   theme(strip.text = element_text(size = 50), 
@@ -325,11 +287,37 @@ b<- ggplot() +
         legend.title = element_blank(),
        legend.position = "top", 
         plot.margin = margin(0.2,1,0,1.35, "cm"))                     
-                     
-c<-ggplot(data=CE[CE$metric=="zOTU",]) + 
+
+#######################################################
+                         
+jpeg("Figures/NMDS-turnover.jpg", width=2000, height=2000) 
+plot_grid(a,b,c,d, nrow=2, ncol=2, rel_widths=c(1.3, .9), rel_heights=c(1,1))                         
+dev.off()                         
+ 
+                         
+                         
+                         
+                         
+                         
+                         
+                         
+                         
+                         
+                         
+                         
+######################################################                         
+                         
+#Bray curtis center-edge pairs only
+CE<-merge(CE, richness, by.x="log_dist", by.y="ï..ID")
+
+#Fix spelling error on sheet before proceeding
+CE$Site<-gsub("Stainbeck","Stainback",as.character(CE$Site))                              
+
+jpeg("Figures/turnover-edge-center.jpg", width=1000, height=1000)                       
+ggplot(data=CE[CE$metric=="zOTU",]) + 
   geom_smooth(method='lm', aes(x=Area, y=dist), colour="black", size=1, alpha=0.20)+
   geom_point(aes(x=Area, y=dist), colour="#6699CC", fill="#332288", alpha=0.70, size=8, shape=21, stroke=7) + 
-  labs(title="C.", x="Kipuka area ("~m^2~")", y="zOTU beta diversity") +
+  labs(x="Kipuka area ("~m^2~")", y="zOTU beta diversity") +
   KipukaTheme +
   #coord_cartesian(ylim=c(0.4, 1))+
   guides(colour="none")+
@@ -345,16 +333,57 @@ c<-ggplot(data=CE[CE$metric=="zOTU",]) +
         rgb(105, 105, 105, maxColorValue = 255),
         linetype = "dotted", 
         size = 0.5), 
-       axis.title=element_text(size=45), 
+       axis.title.y=element_text(size=45), 
+       axis.title.x=element_text(size=45, margin=margin(-25,0,0,0)), 
         axis.text = element_text(size=40), 
         plot.title=element_text(size=45), 
         legend.text=element_text(size=40), 
         legend.title = element_text(size=40),
-       legend.position = "top")                                   
+       legend.position = "top")      
+dev.off()    
                      
-jpeg("Figures/NMDS-turnover-zOTU.jpg", width=3000, height=1000) 
-plot_grid(a,b,c, nrow=1, rel_widths=c(1.3, .9, 1), rel_heights=c(1,1,1))                         
-dev.off()                     
+                     
+                     
+                     
+                     
+                     
+                     
+                     
+                     
+                     
+                     
+                     
+                     
+                     
+
+f<-ggplot(data=CE[CE$metric=="3% OTU",]) + 
+  geom_smooth(method='lm', aes(x=Area, y=dist), colour="black", size=1, alpha=0.20)+
+  geom_point(aes(x=Area, y=dist), colour="#6699CC", fill="#332288", alpha=0.70, size=8, shape=21, stroke=7) + 
+  labs(title="B.", x="Kipuka area ("~m^2~")", y="3% OTU beta diversity") +
+  KipukaTheme +
+  #coord_cartesian(ylim=c(0.4, 1))+
+  guides(colour="none")+
+  scale_x_continuous(trans='log10',
+                     breaks=trans_breaks('log10', function(x) 10^x),
+                     labels=trans_format('log10', math_format(10^.x)))  +                   
+  theme(strip.text = element_text(size = 45), 
+        panel.grid.major = element_line(
+        rgb(105, 105, 105, maxColorValue = 255),
+        linetype = "dotted", 
+        size=1),   
+      panel.grid.minor = element_line(
+        rgb(105, 105, 105, maxColorValue = 255),
+        linetype = "dotted", 
+        size = 0.5), 
+       axis.title.y=element_text(size=45), 
+       axis.title.x=element_text(size=45, margin=margin(-25,0,0,0)), 
+        axis.text = element_text(size=40), 
+        plot.title=element_text(size=45), 
+        legend.text=element_text(size=40), 
+        legend.title = element_text(size=40),
+       legend.position = "top")                                            
+                     
+                    
                
                      
                      
