6dd2a0b0f2cbdc01b9990bbc154e8a7419ec7fbb
steigeec <40217381+steigeec@users.noreply.github.com>
2024-06-20 22:10:10 -0700

    Update Proportional_Represent.R

--- Proportional_Represent.R
+++ Proportional_Represent.R
@@ -1,8 +1,3 @@
-#Project: Kipukas                 #
-#Script: Proportional_Represent.R #
-#Author: Emma Steigerwald         #
-#Date:17 Feb 2022                 #
-###################################
 
 #Set up my working environment
 setwd("G:/My Drive/Kipuka")
