cbfcdb99afd6480ce94e4f3c77bb8b2c41b47da5
steigeec <40217381+steigeec@users.noreply.github.com>
2024-06-20 22:09:28 -0700

    Update NMDS_betaSummary.R

--- NMDS_betaSummary.R
+++ NMDS_betaSummary.R
@@ -1,8 +1,3 @@
-#Project: Kipukas               #
-#Script: NMDS.R                 #
-#Author: Emma Steigerwald       #
-#Date:17 Feb 2022               #
-#################################
 
 #Set up my working environment
 setwd("G:/My Drive/Kipuka/Data")
