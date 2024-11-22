4e9c3c706861e6c0042dbc1c4aa6b5a5704350a1
steigeec <40217381+steigeec@users.noreply.github.com>
2024-11-21 17:20:25 -0800

    Update and rename calculate_zOTU_distance_matrices.R to calculate_OTU_distance_matrices.R

--- calculate_OTU_distance_matrices.R
+++ calculate_OTU_distance_matrices.R
@@ -3,6 +3,7 @@ setwd("H:/My Drive/Kipuka/Data")
 library(reshape2)
 library(tidyverse)
 library(vegan)
+library(dplyr)
 
 otu <- read.csv("OTUs.csv")
                    
@@ -10,9 +11,36 @@ OTU <- otu[14:nrow(otu), 29:ncol(otu)]
 rownames(OTU) <- OTU[,1]
 OTU <- as.data.frame(t(OTU[,-1]))
 names(OTU)[1:2] <- c("OTU", "zOTU")
-
 # Exclude blank (NA or empty string) values 
 OTUtoKeep <- OTU[grepl("OTU", OTU[[1]]), ]
+OTUtoKeep[3:ncol(OTUtoKeep)] <- lapply(OTUtoKeep[3:ncol(OTUtoKeep)], as.numeric)
+
+
+
+# OTU
+OTU3 <- OTUtoKeep %>%
+  group_by(OTU) %>%
+  summarise(across(2:(ncol(OTUtoKeep)-1), sum, na.rm = TRUE)) # Sum columns 3 to last
+OTU3 <- OTU3[,-1]
+OTU3 <- as.data.frame(t(OTU3))
+OTU3[] <- lapply(OTU3, as.numeric)
+
+zOTUbeta <- vegdist(OTU3, method="bray", binary=FALSE, diag=FALSE, upper=FALSE, na.rm=T)
+zOTUbeta<-as.matrix(zOTUbeta)
+zOTUbeta<-as.data.frame(zOTUbeta)
+dist_long <- melt(as.matrix(zOTUbeta))
+write.csv(dist_long, "OTU3_Bray.csv", quote=F, row.names=F)
+
+zOTUbeta <- vegdist(OTU3, method="jaccard", binary=FALSE, diag=FALSE, upper=FALSE, na.rm=T)
+zOTUbeta<-as.matrix(zOTUbeta)
+zOTUbeta<-as.data.frame(zOTUbeta)
+dist_long <- melt(as.matrix(zOTUbeta))
+write.csv(dist_long, "OTU3_jaccard.csv", quote=F, row.names=F)
+
+
+
+
+# zOTU
 # Identify the values in column 1 that occur more than once
 values_to_keep <- names(which(table(OTUtoKeep[[1]]) > 1))
 
